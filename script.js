a25bd28614d708d3b54d8765b5b251d15dc2ff33
Bartelus <wilroed@gmail.com>
2024-08-28 13:26:07 +0200

    div gaming koding

--- script.js
+++ script.js
@@ -20,18 +20,85 @@ console.log(c)
 
 let william = { 
     name: "William White",
+    hp: 100,
     age: 32,
     alive: true,
 }
+
 console.log(william)
 
 console.log(william.name)
 
-if (william.alive)
+
+
+
+let gameRunning = true
+while (gameRunning)
+
 {
-    console.log("han lever!")
+// render game state
+// read user input
+// update game state with user input
+// make AI move 
+}
+
+
+// game loop
+
+let repetitionCount = 0
+
+while (repetitioncount < 10)
+{
+    console.log(repetitioncount)
+    repetitionCount = repetitionCount + 1
+}
+
+// hp redution
+
+while (william.hp > 0)
+
+{
+let random = Math.random () * 10
+if (random < 0.5) {
+    console.log("player taking damage")
+    william.hp = william.hp - 1
+}
+
+if (william.hp <= 0) {
+    console.log("william er dau")
 }
 else
-{
-    console.log("han er dau!")
+    console.log("william lever")
 }
+
+
+/** Under er et eksempel på hvordan kode hvordan variabler varierer ut ifra andre verdier.
+ *   
+ * En lternativ måte å kode dette på er: let isGameOver = william.alive ? "this is true" : "this is false"
+*/
+
+let isGameOver = false
+
+
+
+if (william.alive)
+
+
+{
+    console.log("han lever!")
+    isGameOver = false
+}
+
+else
+
+{ 
+    console.log("han er dau!")
+    isGameOver = true
+}
+
+
+
+
+
+
+console.log(hp)
