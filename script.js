03ddc13d58782d0a0404fabe4e0478bcdba65821
Bartelus <wilroed@gmail.com>
2024-08-28 12:05:25 +0200

    enkel-console-log

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('Hello!');
+let greeting = "Hello Butthead!"
+
+console.log(greeting);
+
+/** 
+ * kommentar
+ * over
+ * flere
+ * linjer
+ */
+
+//tall variabler
+
+let a = 10
+let b = 32
+let c = a + b
+console.log(c)
+
+//egendefinert variabel
+
+let william = { 
+    name: "William White",
+    age: 32,
+    alive: true,
+}
+console.log(william)
+
+console.log(william.name)
+
+if (william.alive)
+{
+    console.log("han lever!")
+}
+else
+{
+    console.log("han er dau!")
+}
